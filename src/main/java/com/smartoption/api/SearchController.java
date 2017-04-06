4a2a7736503aa32c80c3664dd20183a630126073
creativeprogrammer2020 <creativeprogrammer2020@gmail.com>
2017-04-07 00:43:18 +0300

    join spring framework

---
+++ src/main/java/com/smartoption/api/SearchController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.smartoption.api;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SearchController {
+    @RequestMapping("/hello")
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return "Hello "+name;
+    }
+}

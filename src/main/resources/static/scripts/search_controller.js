02d0061635b05f7ccce784e4d75f3f6d5ec72985
creativeprogrammer2020 <creativeprogrammer2020@gmail.com>
2017-04-10 06:00:46 +0300

    interface

--- src/main/resources/static/scripts/search_controller.js
+++ src/main/resources/static/scripts/search_controller.js
@@ -1,16 +1,22 @@
 'use strict';
 angular.module('app')
-       .controller('search_controller', ['$scope', '$window','search_factory', function ($scope,$window, search_factory) {
+       .controller('search_controller', ['$scope','$window','search_factory', function ($scope,$window, search_factory) {
        
-     init();
    
-    function init() {}
-   
-    $scope.getResult= function() {
-        search_factory.Search($scope.skey)
+  
+    $scope.init=function() {
+         $scope.Search="World!";
+   $scope.Count=10;
+   $scope.Tag="All";
+    };
+    $scope.getResult=function(){
+         $scope.Result=search_factory.getResult();
+    };
+    $scope.submit= function() {
+        search_factory.Search($scope.Search,$scope.Count,$scope.Tag)
             .then(function (response) {
-                $scope.Result = response.data;
-         $window.location.href = './#/result';
+                $scope.Result = response;
+         $window.location.href = '/#/result';
             }, function (error) {
                 $scope.Status = 'Unable to load customer data: ' + error.message;
             });
